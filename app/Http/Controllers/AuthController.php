cf0be256d0d12fc18217fbd30e4056b288393224
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2026-01-27 17:11:50 +0200

    Extracting views into the controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,6 +10,16 @@ use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
 {
+    public function showLogin()
+    {
+        return view('auth.login');
+    }
+
+    public function showRegister()
+    {
+        return view('auth.register');
+    }
+
     public function register(Request $request)
     {
         $data = $request->validate([
